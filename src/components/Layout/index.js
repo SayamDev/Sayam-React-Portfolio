12bd72c223b8c8af191d39d89133bea3e4c90b68
Sayam Ajmal <itssanesayam@gmail.com>
2022-05-21 18:12:01 +0100

    changed the css to the correct file

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,23 @@
 import './index.css'
 import { Sidebar } from '../Sidebar'
+import { Outlet } from 'react-router-dom'
 
 export const Layout = () => {
   return (
-    <>
+    <div className='App'>
       <Sidebar />
-    </>
+      <div className='page'>
+      <span className='tags top-tags'>&alt;body&gt;</span>
+
+      <Outlet />
+
+      <span className='tags bottom-tags'>
+      &alt;body&gt;
+      <br />
+      <span className='bottom-tag-html'>&alt;html&gt;</span>
+      </span>
+
+      </div>
+    </div>
   )
 }
