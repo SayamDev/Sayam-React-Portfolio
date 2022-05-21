47a84fe674a568aea4541565a367c9b15bebd956
Sayam Ajmal <itssanesayam@gmail.com>
2022-05-21 18:03:52 +0100

    color to icons

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,6 +4,12 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import {
+    faLinkedin,
+    faGithub,
+    faYoutube,
+    faSkype,
+  } from '@fortawesome/free-brands-svg-icons'
 
 //creating sidebar that will be imported inside the Layout
 
@@ -41,7 +47,29 @@ export const Sidebar = () => {
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
-      
+
+      <ul>
+        <li>
+          <a target="_blank" rel="noreffer" href="www.linkedin.com">
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreffer" href="www.linkedin.com">
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreffer" href="www.linkedin.com">
+            <FontAwesomeIcon icon={faYoutube} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a target="_blank" rel="noreffer" href="www.linkedin.com">
+            <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   )
 }
